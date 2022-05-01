2e77d58423f55aae63321cc6ae4d9b223b152a28
nandobfer <nandobfer@yahoo.com.br>
2022-04-30 22:48:20 -0300

    removed pong testing

--- main.py
+++ main.py
@@ -1,75 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import kivy
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.properties import (
-    NumericProperty, ReferenceListProperty, ObjectProperty
-)
-from kivy.vector import Vector
-from kivy.clock import Clock
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 
+from kivy.uix.button import Button
 
-class PongPaddle(Widget):
-    score = NumericProperty(0)
+kivy.require('1.9.1')
 
-    def bounce_ball(self, ball):
-        if self.collide_widget(ball):
-            vx, vy = ball.velocity
-            offset = (ball.center_y - self.center_y) / (self.height / 2)
-            bounced = Vector(-1 * vx, vy)
-            vel = bounced * 1.1
-            ball.velocity = vel.x, vel.y + offset
-
-
-class PongBall(Widget):
-    velocity_x = NumericProperty(0)
-    velocity_y = NumericProperty(0)
-    velocity = ReferenceListProperty(velocity_x, velocity_y)
-
-    def move(self):
-        self.pos = Vector(*self.velocity) + self.pos
-
-
-class PongGame(Widget):
-    ball = ObjectProperty(None)
-    player1 = ObjectProperty(None)
-    player2 = ObjectProperty(None)
-
-    def serve_ball(self, vel=(4, 0)):
-        self.ball.center = self.center
-        self.ball.velocity = vel
-
-    def update(self, dt):
-        self.ball.move()
-
-        # bounce of paddles
-        self.player1.bounce_ball(self.ball)
-        self.player2.bounce_ball(self.ball)
-
-        # bounce ball off bottom or top
-        if (self.ball.y < self.y) or (self.ball.top > self.top):
-            self.ball.velocity_y *= -1
-
-        # went of to a side to score point?
-        if self.ball.x < self.x:
-            self.player2.score += 1
-            self.serve_ball(vel=(4, 0))
-        if self.ball.right > self.width:
-            self.player1.score += 1
-            self.serve_ball(vel=(-4, 0))
-
-    def on_touch_move(self, touch):
-        if touch.x < self.width / 3:
-            self.player1.center_y = touch.y
-        if touch.x > self.width - self.width / 3:
-            self.player2.center_y = touch.y
-
-
-class PongApp(App):
+var = 0
+def soma_um(instance):
+    global var
+    var += 1
+    instance.text = str(var)    
+    
+class MeuApp(App):
     def build(self):
-        game = PongGame()
-        game.serve_ball()
-        Clock.schedule_interval(game.update, 1.0 / 60.0)
-        return game
-
-
+        layout = BoxLayout(orientation='vertical',
+                padding=[40, 20, 40, 20])
+        
+        layout.add_widget(Label(text='Olá do Kivy!'))
+        btn = Button(text='Pressione-me!', size=(100,50))
+        
+        btn.bind(on_press=soma_um)
+        layout.add_widget(btn)
+        return layout 
+    
 if __name__ == '__main__':
-    PongApp().run()
+    MeuApp().run()
