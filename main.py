138e3bc2db51a4da6dd98180cf9569a7ea5f4c72
nandobfer <nandobfer@yahoo.com.br>
2022-05-01 03:45:25 -0300

    fized window size typo

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from kivy.properties import (
 from kivy.core.window import Window
 
 width, height = 720 / 2, 1280 / 2
-Window.size = screen_size
+Window.size = (width, height)
 
 class SumSymbol(Widget):
     pass
