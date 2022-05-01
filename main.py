1a4b6e21402ae4ac1603371157f9c5d698704ec7
nandobfer <nandobfer@yahoo.com.br>
2022-05-01 12:40:27 -0300

    changed screen size values

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from kivy.properties import (
 )
 from kivy.core.window import Window
 
-width, height = 720 / 2, 1280 / 2
+width, height = 720, 1280 
 # Window.size = (width, height)
 
 class SumSymbol(Widget):
